2d1ca47e13050a0f3ef339e5e5a8dfa905cd2b37
Alexander-RG <alexander.rgareca@gmail.com>
2025-04-01 21:18:03 -0400

    Implementacion de Servicio Hola Alexander

---
+++ ApiGrupo5App/ApiPruebas/Controllers/AlexanderController.cs
@@ -0,0 +1,32 @@
+﻿using ApiPruebas.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPruebas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlexanderController : ControllerBase
+    {
+        private readonly IAlexanderService _alexanderServices;
+
+        public AlexanderController(IAlexanderService alexanderServices)
+        {
+            _alexanderServices = alexanderServices;
+        }
+
+        [HttpGet("Saludo")]
+        public string GetSaludoAlexander() 
+        {
+            try
+            {
+                var response = _alexanderServices.SaludoAlexander();
+                return response;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("Error:",ex);
+            }
+        }
+    }
+}
